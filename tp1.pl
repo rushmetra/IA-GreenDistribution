b1b7a481f7b2b32c65903fc173f9c8097d5fec3f
Rodrigo Rodrigues <55801579+webst2r@users.noreply.github.com>
2021-11-16 21:45:16 +0000

    Base para início do código

---
+++ tp1.pl
@@ -0,0 +1,92 @@
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% SIST. REPR. CONHECIMENTO E RACIOCINIO - LEI/3 - FASE 1
+
+% GRUPO 41
+% Rodrigo Pires Rodrigues  - A93201
+% Daniel Torres Azevedo - A93324
+% Rui Guilherme Monteiro  - A93179
+% Nuno Miguel Sanches Rocha Peixoto - A93244
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% SICStus PROLOG: Declaracoes iniciais
+
+:- set_prolog_flag(discontiguous_warnings, off).
+:- set_prolog_flag(single_var_warnings, off).
+:- set_prolog_flag(unknown, fail).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% SICStus PROLOG: definicoes iniciais
+
+:- op( 900,xfy,'::' ).
+:- dynamic cliente/2.
+:- dynamic estafeta/2.
+:- dynamic encomenda/3.
+:- dynamic entrega/6.
+
+solucoes(T,Q,S) :- findall(T,Q,S).
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+%-------------------------- Invariantes ------------------------------
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+
+
+
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+%----------------------- Base de Conhecimento  - - - -  -  -  -  -   -
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+%-------- Cliente ----------------- - - - - - - - - - -  -  -  -  -   -
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Cliente: #idCliente, Morada -> {V,F}
+
+cliente(1, ‘Rua do Meio’).
+cliente(2, ‘Rua do Verde’).
+cliente(3, ‘Rua do Amarelo’).
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+%-------- Estafeta ---------------- - - - - - - - - - -  -  -  -  -   -
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Estafeta: #idEstafeta, Nome  -> {V,F}
+
+estafeta(0, ‘Daniel’).
+estafeta(1, ‘Nuno’).
+estafeta(2, ‘Guilherme’).
+estafeta(3, ‘Rodrigo’).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+%-------- Encomenda --------------- - - - - - - - - - -  -  -  -  -   -
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Encomenda: #IdEncomenda, Peso, Volume -> {V,F}
+
+encomenda(0, 2.5, 10).
+encomenda(1, 5.2, 15).
+encomenda(2, 19.1, 30).
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+%-------- Entrega --------------- - - - - - - - - - -  -  -  -  -   -
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Entrega: #Data, IdEntrega, IdCliente, IdEstafeta, MeioTransporte, Custo -> {V,F}
+entrega(data(13, 12, 2021), 1, 2, 4, ‘Bicicleta’,  19.99).
+entrega(data(16, 11, 2021), 2, 9, 3, ‘Mota’, 29.99).
+entrega(data(18, 09, 2021), 3, 5, 2, ‘Carro’, 39.99).
+entrega(data(19, 05, 2021), 4, 8, 4, ‘Bicicleta’, 19.99).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+%---------------------- Predicados Solicitados - - - -  -  -  -  -   -
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+%---------- 1. identificar estafeta que utilizou mais vezes um meio de transporte mais ecológico   -  -  -  -   -
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
