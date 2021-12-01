dc336f463029131b2d0b5beda06d43ea40ef6f42
Daniel Azevedo <62104490+danieltazevedo@users.noreply.github.com>
2021-12-01 19:00:53 +0000

    Add files via upload

--- tp1.pl
+++ tp1.pl
@@ -18,6 +18,7 @@
 % SICStus PROLOG: definicoes iniciais
 
 :- op( 900,xfy,'::' ).
+:- dynamic freguesia/2.
 :- dynamic cliente/2.
 :- dynamic estafeta/3.
 :- dynamic encomenda/3.
@@ -49,24 +50,34 @@ solucoes(T,Q,S) :- findall(T,Q,S).
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
-%-------- Cliente ----------------- - - - - - - - - - -  -  -  -  -   -
-%--------------------------------- - - - - - - - - - -  -  -  -  -   -
-% Cliente: #idCliente, idFreguesia -> {V,F}
-
-cliente(1, 0).
-cliente(2, 1).
-cliente(3, 1).
-cliente(4, 3).
-
-%--------------------------------- - - - - - - - - - -  -  -  -  -   -
 %-------- Freguesia ----------------- - - - - - - - - - -  -  -  -  -   -
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Freguesia: #id, nome -> {V,F}
 
 freguesia(0,'Braga').
 freguesia(1,'Porto').
-freguesia(2,'Famalição').
-freguesia(3,'Viana do Castelo').
+freguesia(2,'São Vicente').
+freguesia(3,'Lamaçães').
+freguesia(4,'Gualtar').
+freguesia(5,'Ferreiros').
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+%-------- Cliente ----------------- - - - - - - - - - -  -  -  -  -   -
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Cliente: #idCliente, idFreguesia -> {V,F}
+
+cliente(1, 0).
+cliente(2, 1).
+cliente(3, 2).
+cliente(4, 3).
+cliente(5, 4).
+cliente(6, 5).
+cliente(7, 2).
+cliente(8, 1).
+cliente(9, 3).
+cliente(10, 2).
+cliente(11, 4).
+cliente(12, 0).
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 %-------- Estafeta ---------------- - - - - - - - - - -  -  -  -  -   -
@@ -77,37 +88,68 @@ estafeta(0, 4.1 ,'Daniel').
 estafeta(1, 3.9 ,'Nuno').
 estafeta(2, 3.5 ,'Guilherme').
 estafeta(3, 2.3 ,'Rodrigo').
-estafeta(4, 5.0,'Nunão').
+estafeta(4, 5.0 ,'João').
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 %-------- Encomenda --------------- - - - - - - - - - -  -  -  -  -   -
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Encomenda: #IdEncomenda, Peso, Volume -> {V,F}
 
-encomenda(0, 2.5, 10).
+encomenda(0, 26.5, 10).
 encomenda(1, 5.2, 15).
-encomenda(2, 19.1, 30).
+encomenda(2, 22.1, 30).
 encomenda(3, 15.3, 12).
+encomenda(4, 22, 16).
 encomenda(5, 7.4, 30).
+encomenda(6, 10, 19).
+encomenda(7, 2, 14).
+encomenda(8, 25, 33).
+encomenda(9, 3, 12).
+encomenda(10, 26, 42).
+encomenda(11, 5.5,4).
+encomenda(12, 7,11).
+encomenda(13, 22, 40).
+encomenda(14, 1, 2).
+encomenda(15, 7, 4.5).
+encomenda(16, 25,55).
+encomenda(17, 16, 22).
+encomenda(18, 1.5, 2.5).
+encomenda(19, 18, 14).
+encomenda(20, 5.5, 12). 
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
-%-------- Entrega --------------- - - - - - - - - - -  -  -  -  -   -
+%-------- Entrega --------------- - - - - - - - - - -  -  -  -  -   - 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Entrega: #Data, Prazo, #IdEntrega, #IdEncomenda, #IdCliente, #IdEstafeta, IdVeiculo, Custo, PontuacaoEntrega  -> {V,F}
-entrega(data(13, 12, 2021), 14.0, 0, 1, 2, 4, 1,  19.99,3). %query2(2,[1,2],L).
-entrega(data(13, 12, 2021), 24.0, 1, 2, 2, 3, 1,  13.99,4). 
-entrega(data(19, 05, 2021), 16.0, 2, 3, 1, 4, 2, 15.99,2).
-entrega(data(16, 11, 2021), 20.0, 3, 2, 3, 3, 2, 29.99,5).
-entrega(data(18, 09, 2021), 60.0, 4, 3, 4, 2, 3, 39.99,4).
+entrega(data(12,2,2021), 2.0,  1,  20,  2,1,2,11,4).
+entrega(data(3,6,2021),  6.5,  2,  19,  3,2,2,7,3).
+entrega(data(28,6,2021), 9.0,  3,  18,  1,4,1,5,4).
+entrega(data(1,8,2021),  5.0,  4,  17,  6,3,2,7,2).
+entrega(data(5,4,2021),  7.5,  5,  16,  8,0,3,9,4).
+entrega(data(1,1,2021),  1.5,  6,  15,  9,2,2,11,3).
+entrega(data(5,7,2021),  24.0, 7,  14,  10,0,1,5,2).
+entrega(data(12,2,2021), 12.0, 8,  13,  11,1,3,8,4).
+entrega(data(29,12,2021),4.5,  9,  12,  5,3,2,9,1).
+entrega(data(20,10,2021),3.0,  10, 11,  4,4,2,9,5).
+entrega(data(4,4,2021),  5.5,  11, 10,  7,1,3,9,3).
+entrega(data(1,7,2021),  0.5,  12, 9,   12,3,1,10,4).
+entrega(data(4,6,2021),  6.0,  13, 8,   7,3,3,9,3).
+entrega(data(19,11,2021),9.0,  14, 7,   3,0,1,5,4).
+entrega(data(7,10,2021), 1.5,  15, 6,   12,4,2,9,5).
+entrega(data(5,9,2021),  48.0, 16, 5,   4,1,2,6,5).
+entrega(data(22,4,2021), 1.0,  17, 4,   1,2,3,13,3).
+entrega(data(19,8,2021), 6.5,  18, 3,   7,4,2,7,4).
+entrega(data(9,9,2021),  14.0, 19, 2,   4,0,3,8,5).
+entrega(data(27,6,2021), 7.0,  20, 1,   6,1,3,9,1).
 
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 %-------- Veiculo --------------- - - - - - - - - - -  -  -  -  -   -
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Veiculo: #IdVeiculo, TipoVeiculo -> {V,F}
-veiculo(1,'Bicicleta').
-veiculo(2,'Mota').
-veiculo(3,'Carro').
+veiculo(1,'Bicicleta'). 
+veiculo(2,'Mota').      
+veiculo(3,'Carro').      
 
 
 
@@ -120,8 +162,37 @@ inserir(Termo) :- retract(Termo), !, fail.
 %------------------------------ Registos ------------------------------
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Registar Entrega
-registaEntrega(Data, Prazo, IdE, IdEnc, IdC, IdEst, P, Mt, C,Pe) :-
-             inserir(entrega(Data, Prazo, IdE,IdEnc, IdC, IdEst, P, Mt, C,Pe)).
+
+%prazo >=8 ->3
+% prazo  5-8 -> 4
+% prazo 2->5 -> 6
+%prazo <=2 -> 8
+
+%bicicleta -> 2
+% mota -> 3
+% carro -> 5
+
+preco_veiculo(IdV,R) :- IdV==1, R is 2.
+preco_veiculo(IdV,R) :- IdV==2, R is 3.
+preco_veiculo(IdV,R) :- IdV==3, R is 5.
+
+preco_prazo(Prazo,R) :- Prazo =<2, R=8.
+preco_prazo(Prazo,R) :- Prazo >2, Prazo =< 5, R=6.
+preco_prazo(Prazo,R) :- Prazo >5, Prazo =< 8, R=4.
+preco_prazo(Prazo,R) :- Prazo >8, R=3.
+
+busca_peso(IdEnc,R) :- solucoes(Peso,(encomenda(IdEnc, Peso,_)),List),
+                        head(List,R).
+
+det_veiculo(Peso,R) :- Peso =<5, R is 1.
+det_veiculo(Peso,R) :- Peso >5, Peso =< 20, R is 2.
+det_veiculo(Peso,R) :- Peso > 20, Peso =< 100, R is 3.
+
+
+registaEntrega(Data, Prazo, IdE, IdEnc, IdC, IdEst,Pe):-
+            busca_peso(IdEnc,Peso), det_veiculo(Peso,IdVeiculo), 
+            preco_prazo(Prazo,R1),preco_veiculo(IdVeiculo,R2), C is R1+R2,
+            inserir(entrega(Data, Prazo, IdE,IdEnc, IdC, IdEst, IdVeiculo, C,Pe)).
 
 % Registar Cliente
 registaCliente(IdCliente, IdFreguesia) :- inserir(cliente(IdCliente, IdFreguesia)).
@@ -132,14 +203,13 @@ registaEstafeta(IdEstafeta, Pontuacao, Nome) :- inserir(estafeta(IdEstafeta,Pont
 % Registar Encomenda
 registaEncomenda(IdEncomenda, Peso, Volume) :- inserir(encomenda(IdEncomenda, Peso, Volume)).
 
+% Registar Freguesia
+registaFreguesia(Id, Nome) :- inserir(freguesia(Id,Nome)).
+
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Mostrar registos
 mostrarRegistos(P) :- listing(P).
-
-% Lista de veiculos incluídos na fase 1
-veiculos(['Bicicleta','Mota','Carro']).
-
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
@@ -205,7 +275,7 @@ query4(data(DD,MM,AA),R):- solucoes(Custo,(entrega((data(DD,MM,AA)),_,_,_,_,_,_,
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 %---------- 5. Identificar as zonas com maior volume de entregas  -
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
-query5(N,R):-solucoes(freguesia(IdF,Cidade),(freguesia(IdF,Cidade),entrega(_, _, _,_, IdCliente, _, _, _,_),cliente(IdCliente, IdF)),LFreg1),
+query5(N,R):-solucoes(freguesia(IdF,Nome),(freguesia(IdF,Nome),entrega(_, _, _,_, IdCliente, _, _, _,_),cliente(IdCliente, IdF)),LFreg1),
             diferentes( LFreg1,LFreg2),
             lista_ocorrencias(LFreg1,LFreg2,Ocorrencias),
             busca(N,Ocorrencias,LFreg2,R).
@@ -257,6 +327,7 @@ query8(data(DD1,MM1,AA1),data(DD2,MM2,AA2),R) :-solucoes(D,(entrega(D,_,_,_,_,_,
 
 entre_datas(D1,D2,D3):-anterior(D1,D3),posterior(D2,D3).
 
+%Função que pega numa lista de datas de filtras as que pertencem a um determinado intervalo
 filtra_lista([],_,_,[]).
 filtra_lista([X|T],D1,D2,[X|T2]) :- entre_datas(D1,D2,X),
                             filtra_lista(T,D1,D2,T2).
@@ -265,8 +336,7 @@ filtra_lista([X|T],D1,D2,R) :- not(entre_datas(D1,D2,X)), filtra_lista(T,D1,D2,R
 %---------- 9. calcular o número de encomendas entregues e não entregues, num determinado periodo de tempo  -
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 query9(data(DD1,MM1,AA1),data(DD2,MM2,AA2),NE,E):-solucoes(D,(entrega(D,_,_,_,_,_,_,_,_)),All),
-                                                    filtra_lista(All,data(DD1,MM1,AA1),data(DD2,MM2,AA2),X1),
-                                                    comprimento(X1,A),
+                                                    comprimento(All,A),
                                                     solucoes(Data,(entrega(Data,_,_,IdEnc,_,_,_,_,_),encomenda(IdEnc,_,_)),Entregues),
                                                     filtra_lista(Entregues,data(DD1,MM1,AA1),data(DD2,MM2,AA2),X2),
                                                     comprimento(X2,E),
